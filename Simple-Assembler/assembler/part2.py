2ec455e268ec72e4c972a9f409dca03e2138c764
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-11 00:38:55 +0530

    update

--- Simple-Assembler/assembler/part2.py
+++ Simple-Assembler/assembler/part2.py
@@ -8,12 +8,38 @@ def assemble(command: str) -> str:
 
     reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100",
            "R5": "101", "R6": "110"}
-    if command.split()[0] == "mul":
+    if command.split()[0] == "mul" and len(command.split()) > 4:
         if ((reg.get(command.split()[1]) is not None) and
                 (reg.get(command.split()[2]) is not None) and
                 (reg.get(command.split()[3]) is not None)):
             ans = "00110" + reg.get(command.split()[1]) + reg.get(command.split()[2])\
                   + reg.get(command.split()[3])
+            return ans
+        else:
+            return ""
+
+    elif command.split()[0] == "div" and len(command.split()) > 3:
+        if ((reg.get(command.split()[1]) is not None) and
+                (reg.get(command.split()[2]) is not None)):
+            ans = "00111" + reg.get(command.split()[1]) + reg.get(command.split()[2])
+            return ans
+        else:
+            return ""
+
+    elif command.split()[0] == "rs" and len(command.split()) > 3:
+        if reg.get(command.split()[1]) is not None:
+            temp = ''.join(format(ord(i), '08b') for i in command.split()[2])
+            # print (temp)
+            ans = "01000" + reg.get(command.split()[1]) + temp
+            return ans
+        else:
+            return ""
+
+    elif command.split()[0] == "rs" and len(command.split()) > 3:
+        if reg.get(command.split()[1]) is not None:
+            temp = ''.join(format(ord(i), '08b') for i in command.split()[2])
+            # print (temp)
+            ans = "01000" + reg.get(command.split()[1]) + temp
             return ans
         else:
             return ""
