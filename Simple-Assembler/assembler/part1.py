d39a3be7e29c3c40a9a138946f3aeeec428ff42a
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-08-12 22:59:18 +0530

    minor amend

--- Simple-Assembler/assembler/part1.py
+++ Simple-Assembler/assembler/part1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def assemble(command: str) -> str:
+def assemble(command: str, line_num: int) -> str:
     reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100",
            "R5": "101",
            "R6": "110"}
