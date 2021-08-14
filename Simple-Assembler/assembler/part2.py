727196ff315e9da3af7927aedfebe7c7e8d54ddc
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-14 12:57:15 +0530

    update

--- Simple-Assembler/assembler/part2.py
+++ Simple-Assembler/assembler/part2.py
@@ -9,7 +9,7 @@ def assemble(command: str, line_num: int) -> str:
 
     reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100",
            "R5": "101", "R6": "110"}
-# $ handling
+
     if command.split()[0] == "mul" and len(command.split()) == 4:
         if ((reg.get(command.split()[1]) is not None) and
                 (reg.get(command.split()[2]) is not None) and
@@ -42,7 +42,7 @@ def assemble(command: str, line_num: int) -> str:
             return ""
 
     elif command.split()[0] == "rs" and len(command.split()) == 3:
-        if reg.get(command.split()[1]) is not None:
+        if reg.get(command.split()[1]) is not None and command.split()[2][0:1] == "$":
             ans = "01000" + reg.get(command.split()[1]) + \
                   bin(int(command.split()[2][1:]))[2:]
             return ans
@@ -52,6 +52,13 @@ def assemble(command: str, line_num: int) -> str:
                     print("Line " + str(line_num) + ": ERR: Illegal use of FLAGS")
                     print(command)
                     return ""
+            if command.split()[2][0:1] != "$":
+                print(command)
+                return "Line " + str(line_num) + ":Syntax Error: Illegal Immediate values"
+
+            elif 255 > int(command.split()[2][1:]) or int(command.split()[2][1:]) < 0:
+                print(command)
+                return "Line " + str(line_num) + ": ERR: Wrong Limit used"
             print("Line " + str(line_num) + ": ERR: Wrong register")
             return ""
 
@@ -66,6 +73,15 @@ def assemble(command: str, line_num: int) -> str:
                     print("Line " + str(line_num) + ": ERR: Illegal use of FLAGS")
                     print(command)
                     return ""
+            if command.split()[2][0:1] != "$":
+                print(command)
+                return "Line " + str(
+                    line_num) + ":Syntax Error: Illegal Immediate values"
+
+            elif 255 > int(command.split()[2][1:]) or int(
+                    command.split()[2][1:]) < 0:
+                print(command)
+                return "Line " + str(line_num) + ": ERR: Wrong Limit used"
             print("Line " + str(line_num) + ": ERR: Wrong register")
             return ""
 
