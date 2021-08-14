843edcb976b66542b89e925d4c3f2c4d0897cfca
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-08-14 21:16:50 +0530

    unused bits

--- Simple-Assembler/assembler/part2.py
+++ Simple-Assembler/assembler/part2.py
@@ -57,13 +57,13 @@ def assemble(command: str, line_num: int) -> str:
         if reg.get(command.split()[1]) is not None and command.split()[2][0:1] == "$"\
         and (int(command.split()[2][1:]) >= 0 and (int(command.split()[2][1:]) <= 255)
         and command.split()[2][1:].isnumeric()):
-            if len(bin(int(command.split()[2][1:]))) != 8:
+            if len(bin(int(command.split()[2][1:]))[2:]) != 8:
                 temp = 8 - len(bin(int(command.split()[2][1:])))
-                temp1 = ("0" * temp) + "00"+ (bin(int(command.split()[2][1:]))[2:])
+                temp1 = ("0" * temp) + (bin(int(command.split()[2][1:]))[2:])
                 ans = "01000" + reg.get(command.split()[1]) + temp1
                 return ans
             else:
-                ans = "01000" + reg.get(command.split()[1]) + bin(int(command.split()[2][1:]))
+                ans = "01000" + reg.get(command.split()[1]) + bin(int(command.split()[2][2:]))
                 return ans
         else:
             for i in command.split():
@@ -91,15 +91,14 @@ def assemble(command: str, line_num: int) -> str:
         if reg.get(command.split()[1]) is not None and command.split()[2][0:1] == "$" \
                 and (int(command.split()[2][1:]) >= 0 and int(command.split()[2][1:]) <= 255
         and command.split()[2][1:].isnumeric()):
-            if len(bin(int(command.split()[2][1:]))) != 8:
+            if len(bin(int(command.split()[2][1:]))[2:]) != 8:
                 temp1 = ""
                 temp = 8 - len(bin(int(command.split()[2][1:])))
                 temp1 = ("0" * temp) + "00" +(bin(int(command.split()[2][1:]))[2:])
                 ans = "01000" + reg.get(command.split()[1]) + temp1
                 return ans
             else:
-                ans = "01000" + reg.get(command.split()[1]) + bin(
-                    int(command.split()[2][1:]))
+                ans = "01000" + reg.get(command.split()[1]) + bin(int(command.split()[2][1:]))[2:]
                 return ans
         else:
             for i in command.split():
